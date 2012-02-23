442ac575fb26d99e5bd71e0c7dc1534b2b478014
Daniel 'koolfy' Faucon <koolfy@geekmx.org>
2012-02-23 22:23:06 +0100

    Initial work of implementing an actual database.

--- sources.py
+++ sources.py
@@ -15,10 +15,11 @@
 #  You should have received a copy of the GNU General Public License
 #  along with Epic Media Pulse.  If not, see <http://www.gnu.org/licenses/>.
 
-import pickle
+import os
 import glob
 import mutagen
 import mimetypes
+import sqlite3
 
 SUPPORTED_TYPES = [
     'audio/mpeg',
@@ -28,31 +29,37 @@ SUPPORTED_TYPES = [
 
 class Local:
     @classmethod
-    def db_create(self, file):
+    def __init__(self, filename):
+        if (os.path.exists(filename) == False):
+            self.db_create(filename)
+        else:
+            self.directory = filename
+            self.db = sqlite3.connect(self.directory)
+            self.cursor = self.db.cursor()
+
+
+    @classmethod
+    def db_create(self, filename):
         '''Create a void database'''
-        db = {}
-        db_file = open(file, 'wb')
-        pickle.dump(db, db_file)
-        db_file.close()
-        return db
+        self.directory = filename
+        self.db = sqlite3.connect(self.directory)
+        self.cursor = self.db.cursor()
+        self.cursor.execute('''create table artist ( id integer, name text)''')
+        self.cursor.execute('''create table album ( id integer, title text,\
+                   year integer, idartist integer)''')
+        self.cursor.execute('''create table song ( id integer, title text, file text,\
+                    genre integer, idalbum integer, idartist integer)''')
+        
+        self.db.commit()
 
     @classmethod
-    def db_load(self, file):
-        '''Load a database from a file'''
-        db_file = open(file, 'rb')
-        db = pickle.load(db_file)
-        db_file.close()
-        return db
+    def db_close(self):
+        '''Close the active database'''
+        self.db.commit()
+        self.cursor.close()
 
     @classmethod
-    def db_save(self, file, db):
-        '''Save a database to a file'''
-        db_file = open(file, 'wb')
-        pickle.dump(db, db_file)
-        db_file.close()
-
-    @classmethod
-    def db_import(self, folder, db):
+    def db_import(self, folder):
         '''Import all files from a folder to the database'''
         for filename in glob.glob(folder + '/*'):
             filetype = mimetypes.guess_type(filename)[0]
@@ -66,7 +73,10 @@ class Local:
             tags = mutagen.File(filename, easy=True)
             if tags == None:
                 print('ERROR : ' + filename + ' has no ID3 tag, skipping...')
-            else:
-                db[filename] = tags
-
-        return db
+            #else:
+            ## Insert here the code of an actual importation
+            ## in the sqlite database for each song
+            ## - has to check if it is already there
+            ## - has to hack if the artist/album already exists
+            ## - if so, link to them. If not, create and link
+            ## - what if an important tag is missing ? -> UNKNOWN
