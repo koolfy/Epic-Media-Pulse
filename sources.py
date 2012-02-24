702f23da80835d142d45f637cc5d708f36a4424b
Daniel 'koolfy' Faucon <koolfy@geekmx.org>
2012-02-25 00:02:15 +0100

    First unfinished draf of the new db_import

--- sources.py
+++ sources.py
@@ -15,6 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with Epic Media Pulse.  If not, see <http://www.gnu.org/licenses/>.
 
+
 import os
 import glob
 import mutagen
@@ -36,7 +37,10 @@ class Local:
             self.directory = filename
             self.db = sqlite3.connect(self.directory)
             self.cursor = self.db.cursor()
-
+        
+        self.maxSongId = 0
+        self.maxArtistId = 0
+        self.maxAlbumId= 0
 
     @classmethod
     def db_create(self, filename):
@@ -61,6 +65,38 @@ class Local:
     @classmethod
     def db_import(self, folder):
         '''Import all files from a folder to the database'''
+        self.songList = []
+        self.artistList = []
+        self.albumList = []
+        self.albumIdList = []
+        self.artistIdList = []
+        self.artistId = 0
+        self.albumId= 0
+        dblist = ['song', 'artist', 'album']
+        for db in dblist:
+            self.cursor.execute('''select * from ''' + db)
+            for entry in self.cursor:
+                if (db == 'song'): self.songList.append(entry[2])
+                if (db == 'artist'): 
+                    self.artistList.append(entry[1])
+                    self.artistIdList.append(entry[0])
+                else: 
+                    self.albumList.append(entry[1])
+                    self.albumIdList.append(entry[0])
+        
+        self.cursor.execute('''select * from song order by 1 desc limit 1''')
+        for entry in self.cursor:
+                self.maxSongId = entry[0]
+
+        self.cursor.execute('''select * from artist order by 1 desc limit 1''')
+        for entry in self.cursor:
+                self.maxArtistId = entry[0]
+
+        self.cursor.execute('''select * from album order by 1 desc limit 1''')
+        for entry in self.cursor:
+                self.maxAlbumId = entry[0]
+
+
         for filename in glob.glob(folder + '/*'):
             filetype = mimetypes.guess_type(filename)[0]
             
@@ -73,7 +109,49 @@ class Local:
             tags = mutagen.File(filename, easy=True)
             if tags == None:
                 print('ERROR : ' + filename + ' has no ID3 tag, skipping...')
-            #else:
+                continue
+            
+            if filename in self.songList:
+                print('ERROR : ' + filename + ' already in the database,'\
+                    + ' skipping...')
+                continue
+            else:
+                if (tags['artist'][0] in self.artistList):
+                    index = self.artistList.index(tags['artist'][0])
+                    self.artistId = self.artistIdList[index]
+                else:
+                    self.maxArtistId = self.maxArtistId +1
+                    t = (self.maxArtistId, tags['artist'][0])
+                    self.cursor.execute('''insert into artist (id, name) '''\
+                                        + '''VALUES (?, ?)''', t)
+                    self.artistId = self.maxArtistId
+
+                if (tags['album'][0] in self.albumList):
+                    index = self.albumList.index(tags['album'][0])
+                    self.albumId = self.albumIdList[index]
+                else:
+                    self.maxAlbumId = self.maxAlbumId +1
+                    t = (self.maxAlbumId, tags['album'][0])
+                    self.cursor.execute('''insert into album (id, title,'''\
+                                        ''' year, idartist) VALUES'''\
+                                        '''(?, ?, 0, 0)''', t)
+                    self.albumId = self.maxAlbumId
+
+                self.maxSongId = self.maxSongId +1
+                t = [self.maxSongId, tags['title'][0], filename,\
+                     self.albumId, self.artistId ]
+                self.cursor.execute('''insert into song (id, title, file,\
+                    genre, idalbum, idartist)\
+                    VALUES (?, ?, ?, 0, ?, ?)''', t)
+                
+                self.db.commit()
+            
+if __name__ == '__main__':
+        testbase = Local('database')
+        testbase.db_import('Music')
+        testbase.db_close()
+
+            
             ## Insert here the code of an actual importation
             ## in the sqlite database for each song
             ## - has to check if it is already there
